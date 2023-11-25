8c338f99749944108b8905267b3ac679f20bc29b
elizaveta_rybina <liza.rybina@inbox.ru>
2023-11-25 18:29:30 +0300

    php libs

--- src/php/dataBase.php
+++ src/php/dataBase.php
@@ -4,11 +4,11 @@ $dbname = 'dns';
 $charset = 'utf8';
 $dbuser = 'root';
 $dbpass = 'root';
-@$connection = new PDO("mysql:host=$host;dbname=$dbname;charset=$charset", $dbuser, $dbpass);  
+@$connection = new PDO("mysql:host=$host;dbname=$dbname;charset=$charset", $dbuser, $dbpass);
 
-/*$host = 'localhost';
-$dbname = 'cp75730_rybina';
-$charset = 'utf8';
-$dbuser = 'cp75730_rybina';
-$dbpass = 'root';
-@$connection = new PDO("mysql:host=$host;dbname=$dbname;charset=$charset", $dbuser, $dbpass);*/
+require 'libs/autoload.php';
+	@$redis = new Predis\Client([
+		'scheme' => 'tcp',
+		'host'   => 'redis',
+		'port'   => 6379,
+	]);
---
+++ src/php/sign/signin.php
@@ -0,0 +1,47 @@
+<?php
+	require_once("../dataBase.php");
+
+	if(isset($_COOKIE['login']) && isset($_COOKIE['token']))
+	{
+		$login = $_COOKIE['login'];
+		$redistoken = $redis->hget('authentication:' . $login, 'token');
+
+		if($redistoken == $_COOKIE['token'])
+		{
+			$success['Message'] = "Success";
+			die(print(json_encode($success)));
+		}
+	}
+
+	if(isset($_REQUEST['login']) && isset($_REQUEST['password']))
+	{
+		$login = $_REQUEST['login'];
+		$password = $_REQUEST['password'];
+
+		$fetchUsers = $connection->prepare("SELECT `password` FROM `users` WHERE `login` = :userlogin");
+		$fetchUsers->bindValue(':userlogin', $login, PDO::PARAM_STR);
+		$fetchUsers->execute() or die(print_r($fetchUsers->errorInfo()));
+		$existedUsers = $fetchUsers->fetchAll(PDO::FETCH_ASSOC);
+
+		if(count($existedUsers) == 0)
+		{
+			$error['Message'] = "Wrong data";
+			die(print(json_encode($error)));
+		}
+
+		if(password_verify($password, $existedUsers[0]['password']))
+		{
+			$auto_expire = 12 * 60 * 60; // 12 hours
+			$token = password_hash(uniqid(), PASSWORD_DEFAULT);
+			$redis->hset('authentication:' . $login, "token", $token);
+			$redis->expire('authentication', $auto_expire);
+			setcookie('login', $login, time() + $auto_expire, '/');
+			setcookie('token', $token, time() + $auto_expire, '/');
+
+			$success['Message'] = "Success";
+			die(print(json_encode($success)));
+		}
+	}
+
+	$error['Message'] = "Wrong data";
+	die(print(json_encode($error)));
