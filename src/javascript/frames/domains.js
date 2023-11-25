863ba99f6ce00462e888255676bf2608dad207ce
elizaveta_rybina <liza.rybina@inbox.ru>
2023-11-25 15:21:03 +0300

    replacing icons

--- src/javascript/frames/domains.js
+++ src/javascript/frames/domains.js
@@ -3,29 +3,28 @@
 export function domainHelper(responce) {
 
 	var wrapper = $('<div class="form-wrapper"></div>');
-	
+
 	responce.forEach((domain) => {
 		wrapper.append(`
 			<div class="form-body">
 				<p>${domain.name}</p>
 				<div class="buttons">
-					<button class="adding"><img src="image/add.png" /></button>
 					<form action="pages/setting.html">
 						<input type="hidden" name="domainid" value="${domain.id}">
 						<button type="submit">
-							<img src="image/setting.png" />
+              <i class="fa-solid fa-gear" style="color: #000000;"></i>
 						</button>
 					</form>
 					<form class="deleteDomain">
 						<input type="hidden" name="domainid" value="${domain.id}">
 						<button type="submit">
-							<img src="image/delete.png" />
+              <i class="fa-solid fa-trash" style="color: #000000;"></i>
 						</button>
 					</form>
 				</div>
 			</div>
 		`);
 	});
-	
+
 	return wrapper;
 }
